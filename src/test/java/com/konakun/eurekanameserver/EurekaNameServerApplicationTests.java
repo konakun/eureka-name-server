a7584755cae3f585391b1b9396872cb630e502c7
Konakun <Dragonzerker@live.com>
2022-06-16 00:24:11 -0600

    feature: eureka server

---
+++ src/test/java/com/konakun/eurekanameserver/EurekaNameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konakun.eurekanameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
